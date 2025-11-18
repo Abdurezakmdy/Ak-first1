8b0fa15de5c46ad1d92b230b5234c7603748e76d
Abdurezak Mohammed <abdurezakmohammed501@gmail.com>
2025-11-19 00:41:38 +0300

    Update ak first(edited).cpp

--- ak first.cpp
+++ ak first.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main() {
-    double num1, num2, product;
+    double x, y, product;
 
-    cout << "Enter the first number: ";
-    cin >> num1;
+    cout << "Enter x: ";
+    cin >> x;
 
-    cout << "Enter the second number: ";
-    cin >> num2;
+    cout << "Enter y: ";
+    cin >> y;
 
-    product = num1 * num2;
+    product = x * y;
 
     cout << "The product is: " << product << endl;
 
